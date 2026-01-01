257c5042548ed35a9aecb80ecbcbaa2875a3d387
DmytroLavrov <dimalavrov2004.06.16@gmail.com>
2026-01-01 21:29:09 +0200

    feat(breed-filter): create dropdown filter with DogService, URL persistence, skeleton loader, and styling

---
+++ src/app/features/dogs/components/breed-filter/breed-filter.component.ts
@@ -0,0 +1,47 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  EventEmitter,
+  Input,
+  Output,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import { Breed } from '@shared/models/dog.model';
+
+@Component({
+  selector: 'app-breed-filter',
+  imports: [],
+  templateUrl: './breed-filter.component.html',
+  styleUrl: './breed-filter.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BreedFilterComponent {
+  @Input({ required: true }) public breeds: Breed[] = [];
+  @Input() public selectedBreedId: number | null = null;
+  @Input() public isLoading: boolean = false;
+
+  @Output() public breedChange = new EventEmitter<number | null>();
+
+  protected selectedValue: WritableSignal<string> = signal<string>('all');
+
+  constructor() {
+    effect(() => {
+      const breedId = this.selectedBreedId;
+      const value = breedId ? breedId.toString() : 'all';
+
+      this.selectedValue.set(value);
+    });
+  }
+
+  public onBreedChange(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    const value = target.value;
+
+    const breedId = value === 'all' ? null : parseInt(value, 10);
+
+    this.selectedValue.set(value);
+    this.breedChange.emit(breedId);
+  }
+}
